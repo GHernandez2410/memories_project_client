0b0a19dc726313e6dfed7bed2bd0906f2c0caa57
gHernandez2410 <gonzalo.hernandez2410@gmail.com>
2021-02-25 04:26:18 +0200

    Update: refactory of route like post

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 const url = 'https://masatos-memories.herokuapp.com/posts';
 
+// const url = 'http://localhost:5000/posts';
+
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const likePost = (id) => axios.patch(`${url}/likePost/${id}`);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
